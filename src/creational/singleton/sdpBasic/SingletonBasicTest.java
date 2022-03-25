90d6e84af3316de6ece4dc4531297fc80098cde4
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2022-03-25 18:02:05 +0300

    Create SingletonBasicTest.java

---
+++ src/creational/singleton/sdpBasic/SingletonBasicTest.java
@@ -0,0 +1,8 @@
+package creational.singleton.sdpBasic;
+
+public class SingletonBasicTest {
+
+    public static void main(String[] args) {
+        SingletonBasic singletonBasic = SingletonBasic.getSingleton();
+    }
+}
