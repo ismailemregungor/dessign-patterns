0d33d0e677dcb3c1db3004db42ad144dae88402d
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2022-04-13 18:38:59 +0300

    Create SingletonDoubleCheckTest.java

---
+++ src/creational/singleton/sdpDoubleCheck/SingletonDoubleCheckTest.java
@@ -0,0 +1,11 @@
+package creational.singleton.sdpDoubleCheck;
+
+public class SingletonDoubleCheckTest {
+
+    public static void main(String[] args) {
+
+        SingletonDoubleCheck singletonDoubleCheck
+                = SingletonDoubleCheck.getSingletonDoubleCheck();
+
+    }
+}
